75e74dc783b03ecf14ac6c8c823b4a1b2900b8e1
Will Barbour <barbourww@users.noreply.github.com>
2020-10-05 18:35:53 -0500

    Updated defaults with new file naming formatters

--- parameters.py
+++ parameters.py
@@ -15,21 +15,10 @@ PIPE_SINK_NAME_FORMATTER = '{}_sink'
 PIPE_SINGLE_FILESINK_NAME_FORMATTER = '{}_filesink'         # use when filesink is not camera-specific
 PIPE_CAMERA_FILESINK_NAME_FORMATTER = '{}_filesink_{}'      # use for camera-specific filesinks
 
-
-# default amount of video time that is buffered on incoming streams
-# so that the video snapshot can record into history on trigger
-# -----------------------------------------------------------------
-DEFAULT_BUFFER_TIME = 60
-
-# default duration of video snapshot recording when it is triggered
-# this time is NOT added to buffer time when recording
-# -----------------------------------------------------------------
-DEFAULT_VIDEO_SNAP_DURATION = 60
-
 # default filename template for persistent multi-segment recording
-# '{}' denotes camera name; %d denotes segment number (%05d is five-zeros-padded)
-# -------------------------------------------------------------------------------
-DEFAULT_RECORDING_FILENAME = 'recording/record_{}_%05d.mp4'
+# '{cam_name}' denotes camera name; %d denotes segment number (%05d is five-zeros-padded)
+# ---------------------------------------------------------------------------------------
+DEFAULT_RECORDING_FILENAME = 'recording/record_{cam_name}_%05d.mp4'
 
 # default segment duration in minutes for persistent multi-segment recording
 # --------------------------------------------------------------------------
@@ -44,6 +33,16 @@ DEFAULT_NUMBER_STORED_SEGMENTS = 0
 # -----------------------------------------------------
 ESTIMATED_CAMERA_BITRATE = 5.5
 
+# default amount of video time that is buffered on incoming streams
+# so that the video snapshot can record into history on trigger
+# -----------------------------------------------------------------
+DEFAULT_BUFFER_TIME = 60
+
+# default filename template for image snapshots
+# '{cam_name}' denotes camera name and '{datetime_unix}' denotes UNIX time
+# ------------------------------------------------------------------------
+DEFAULT_IMAGE_SNAPSHOT_FILENAME = 'imgsnap/snap_{cam_name}_{datetime_unix}.jpg'
+
 # image encoder spin up time
 # --------------------------
 IMAGE_ENCODE_SPIN_UP = 3.0
@@ -51,6 +50,16 @@ IMAGE_ENCODE_SPIN_UP = 3.0
 # image snap pipeline execute time
 # --------------------------------
 IMAGE_SNAP_EXECUTE_TIME = 2.0
+
+# default duration of video snapshot recording when it is triggered
+# this time is NOT added to buffer time when recording
+# -----------------------------------------------------------------
+DEFAULT_VIDEO_SNAP_DURATION = 60
+
+# default filename template for video snapshots
+# '{datetime_unix}' denotes UNIX time
+# ---------------------------------------------
+DEFAULT_VIDEO_SNAPSHOT_FILENAME = 'vidsnap/snap_{datetime_unix}.jpg'
 
 # default camera stream reporting interval
 # ----------------------------------------
