9d333ab82f5f5c26a4db6f8b9f5e70a9ba06de80
Will Barbour <12778879+barbourww@users.noreply.github.com>
2020-11-18 12:58:23 -0600

    Draft of timestamp parser implemented with command line options
    Needs testing and documentation.

--- query_frames.py
+++ query_frames.py
@@ -6,23 +6,25 @@ import warnings
 from traceback import print_exc
 import re
 import csv
+import time
+import ast
 
 import utilities
 from parameters import *
 
 
-def get_video_stats(video_file_names):
+def get_video_frame_counts(video_file_names):
     """
     Run FFprobe frame count queries for recorded video segments.
-    :param video_file_names: list of video file names to query for frame counts
+    :param video_file_names: list of tuples (video-file-name, segment-number) to query for frame counts
     :return: dictionary of frame counts {video-file-name: frame-count, ...}
     """
     if not isinstance(video_file_names, (list, tuple)):
-        raise TypeError("Must provide list of tuple of video filenames.")
+        raise TypeError("Must provide list of tuples (video-file-name, segment-number).")
     frame_counts = {}
     print("\nRunning video frame count queries.")
     for i, (vfn, vfi) in enumerate(video_file_names):
-        if i % 100 == 0:
+        if i % 500 == 0:
             print("Query number {}".format(i))
         cmd = ["ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=nb_frames",
                "-of", "default=nokey=1:noprint_wrappers=1", vfn]
@@ -37,7 +39,87 @@ def get_video_stats(video_file_names):
     return frame_counts
 
 
-def find_files(recording_directories, file_name_format, camera_names, drop_last_file=False, first_file_index=0):
+def get_video_frame_timestamps(video_file_names):
+    """
+    Run frame timestamp parsing for recorded video segments.
+    :param video_file_names: list of video file names to query for frame counts
+    :return: dictionary of list of frame timestamps {video-file-name: list-frame-timestamps, ...}
+    """
+    if not isinstance(video_file_names, (list, tuple)):
+        raise TypeError("Must provide list of tuples (video-file-name, segment-number).")
+    import pickle
+    with open('./resources/timestamp_geometry_4K.pkl', 'rb') as f:
+        g = pickle.load(f)
+        w = g['w']
+        h = g['h']
+        x0 = g['x0']
+        y0 = g['y0']
+        n = g['n']
+        h13 = g['h13']
+        h23 = g['h23']
+        h12 = g['h12']
+        w12 = g['w12']
+    with open('./resources/timestamp_pixel_checksum_6.pkl', 'rb') as f:
+        dig_cs6 = pickle.load(f)
+    import cv2
+    import numpy as np
+    timestamps = {}
+    pixel_errors = []
+    for i, (vfn, vfi) in enumerate(video_file_names):
+        t0 = time.time()
+        cap = cv2.VideoCapture(vfn)
+        assert cap.isOpened(), "Cannot open file \"{}\"".format(vfn)
+
+        # TODO: pull out camera name and segment number extraction into a utility function
+        cam = vfn.split('record_')[1].split('_')[0]
+        print("Processing camera {}".format(cam))
+        cam_ts = []
+        i = 0
+        while True:
+            ret, frame = cap.read()
+            if frame is None:
+                print("End of video after {} frames.".format(i))
+                break
+            tsimg = frame[0:y0 + h, 0:x0 + (n * w), :]
+            tsgray = cv2.cvtColor(tsimg, cv2.COLOR_BGR2GRAY)
+            ret, tsmask = cv2.threshold(tsgray, 127, 255, cv2.THRESH_BINARY)
+
+            ts_dig = []
+            for j in range(n):
+                if j == 10:
+                    ts_dig.append('.')
+                    continue
+                pixels = tsmask[y0:y0 + h, x0 + j * w:x0 + (j + 1) * w]
+                cs = [[int(pixels[:h13, :w12].sum() / 255), int(pixels[:h13, w12:].sum() / 255)],
+                      [int(pixels[h13:h23, :w12].sum() / 255), int(pixels[h13:h23, w12:].sum() / 255)],
+                      [int(pixels[h23:, :w12].sum() / 255), int(pixels[h23:, w12:].sum() / 255)]
+                      ]
+                cs = np.array(cs)
+                cs_diff = [(dig, abs(cs - cs_ref).sum()) for dig, cs_ref in dig_cs6.items()]
+                pred_dig, pred_err = min(cs_diff, key=lambda x: x[1])
+                if pred_err > 0:
+                    ts_dig.append(0)
+                    pixel_errors.append(cs)
+                else:
+                    ts_dig.append(pred_dig)
+            cam_ts.append(ast.literal_eval(''.join(map(str, ts_dig))))
+            i += 1
+            continue
+        timestamps[vfn] = cam_ts
+        cap.release()
+        print(i / (time.time() - t0), "fps processing rate")
+    # if we had any errors in checksum recognition, append them to the running file
+    if len(pixel_errors) > 0:
+        if 'pixel_errors.pkl' in os.listdir('./resources'):
+            with open('./resources/pixel_errors.pkl', 'rb') as f:
+                pixel_errors = pickle.load(f) + pixel_errors
+        with open('./resources/pixel_errors.pkl', 'wb') as f:
+            pickle.dump(pixel_errors, f)
+    return timestamps
+
+
+def find_files(recording_directories, file_name_format, camera_names, drop_last_file=False, first_file_index=0,
+               filter_filenames=None):
     """
     Determine files in recording directories that match file recording naming format.
     :param recording_directories: list of directories in which to search for files (one or many, based on file naming)
@@ -45,6 +127,7 @@ def find_files(recording_directories, file_name_format, camera_names, drop_last_
     :param camera_names: list of camera names to substitute into file name format
     :param drop_last_file: flag to ignore/drop the last file in the recording sequence, per camera
     :param first_file_index: minimum recording segment number to keep files (used for checking recent files only)
+    :param filter_filenames: list of filters to narrow down filenames (tested by `if filter in filename:`)
     :return: list of file names for recordings matching file name format
     """
     file_name_regex = re.sub('%(0[0-9]{1})*d', '([0-9]+)', file_name_format)
@@ -71,6 +154,8 @@ def find_files(recording_directories, file_name_format, camera_names, drop_last_
         else:
             match_files += sorted(cam_files, key=lambda x: x[1])
     print("Found {} files matching recording file name format.".format(len(match_files)))
+    if filter_filenames is not None:
+        match_files = [fn for fn in match_files if any([fn_filt in fn for fn_filt in filter_filenames])]
     return match_files
 
 
@@ -131,6 +216,23 @@ def write_frame_count_results(results_dict, filename, print_results=False):
             writer.writerow([cn, fc])
             if print_results is True:
                 print(cn, '-->', fc)
+    return
+
+
+def write_frame_timestamp_results(results_dict, filename):
+    """
+    Write the results of video frame timestamp extraction to CSV file. File structure is:
+        filename0; ts0; ts1; ts2; ts3; ...
+        filename1; ts0; ts1; ts2; ts3; ...
+    :param results_dict: dictionary of {filename: [ordered list of timestamps]
+    :param filename: file path where to save results
+    :return: None
+    """
+    with open(filename, 'w') as f:
+        writer = csv.writer(f, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerow(["filename", "timestamps (delimited)"])
+        for fn, fts in sorted(list(results_dict.items()), key=lambda x: x[0]):
+            writer.writerow([fn] + fts)
     return
 
 
@@ -196,17 +298,23 @@ def main(argv):
     usage = """
     query_frames.py [-h] [-l] -s <session-directory>
     
-    # alternate behaviors that execute and exit
+    # primary behavior mode selection (must indicate one primary or alternate mode)
+    -c/--count: count frames using FFmpeg container query
+    -t/--timestamp: parse timestamps in frames using pixel checksum method (currently very slow, consider)
+    
+    # alternate behavior modes
     -h/--help: print usage information, then exit
     -l/--load_plot_output: load results file in session directory (need -s), plot the results, then exit; if non-default
-        results output filename, then specify it with -o/--output_filename= option
+        frame count results output filename, then specify it with -o/--output_filename= option
     
     # required arguments
     -s/--session_directory= /path/to/session_directory : (required) path of the session directory where files are stored
     
     # options with value required (options themselves are not required)
     -o/--output_filename= /path/to/output_file.csv : override output filename for results
-    -i/--first_file= ### : file segment index at which to start querying
+    -f/--first_file= ### : file segment index at which to start querying
+    -i/--input_filename= : comma-delineated list of file name fragments to narrow down video segment files; uses simple 
+        `if 'fragment' in full_file_path:` check, so be specific; e.g., p2c3_00150,p3c1_00004
     -a/--append_outputs= /path/to/alt_output1.csv,path/to/alt_output2.csv : comma-delineated list of *absolute* results 
         file paths to append to -o/--output_filename= specified results (used during post-facto plotting option -l/--.)
     
@@ -217,18 +325,24 @@ def main(argv):
     
     """
     try:
-        opts, args = getopt.getopt(argv, 'hldps:o:i:a:',
-                                   ['help', 'load_plot_output', 'drop_last_file', 'plot_output', 'print_output',
-                                    'session_directory=', 'output_filename=', 'first_file=', 'append_outputs='])
+        opts, args = getopt.getopt(argv, 'cthldps:o:f:i:a:',
+                                   ['count', 'timestamp', 'help', 'load_plot_output',
+                                    'drop_last_file', 'plot_output', 'print_output',
+                                    'session_directory=', 'output_filename=',
+                                    'first_file=', 'input_filename=', 'append_outputs='])
     except getopt.GetoptError:
         print("Usage:", usage)
         print_exc()
         sys.exit(2)
 
+    # defaults for mode choice
+    count_frames = False
+    parse_timestamps = False
     # defaults for inputs
     session_directory = None
     drop_last_file = False
     results_filename = None
+    input_filename_filters = None
     append_results = None
     first_file = 0
     print_output = False
@@ -240,6 +354,10 @@ def main(argv):
         if opt in ('-h', '--help'):
             print("Usage:", usage)
             sys.exit()
+        elif opt in ('-c', '--count'):
+            count_frames = True
+        elif opt in ('-t', '--timestamp'):
+            parse_timestamps = True
         elif opt in ('-l', '--load_plot_output'):
             plot_and_exit = True
         elif opt in ('-s', '--session_directory'):
@@ -257,10 +375,12 @@ def main(argv):
                 if os.path.exists(fn):
                     append_results.append(fn)
                 else:
-                    warnings.warn("Path to results file in append argument does not exit: {}".format(fn))
+                    warnings.warn("Path to results file in append argument does not exist: {}".format(fn))
             if len(append_results) == 0:
                 append_results = None
-        elif opt in ('-i', '--first_file',):
+        elif opt in ('-i', '--input_filename'):
+            input_filename_filters = arg.split(',')
+        elif opt in ('-f', '--first_file',):
             first_file = int(arg)
         elif opt in ('--print_output',):
             print_output = True
@@ -276,15 +396,37 @@ def main(argv):
         sys.exit(2)
     session_info_file_path = os.path.join(session_directory, '_SESSION_INFO.txt')
 
+    # one of these modes must be selected
+    if plot_and_exit is False:
+        if count_frames is False and parse_timestamps is False:
+            print("Must select a mode: count frames (-c), parse timestamps (-t), or load and plot results (-l).")
+            print("Usage:", usage)
+            sys.exit(2)
+
     # default to files in session directory if not specified
     if results_filename is None:
-        results_filename = os.path.join(session_directory, 'frame_counts_recording.csv')
-        plot_filename = os.path.join(session_directory, 'frame_counts_recording.pdf')
+        count_filename = os.path.join(session_directory, 'frame_counts_recording.csv')
+        timestamp_filename = os.path.join(session_directory, 'frame_timestamp_recording.csv')
     else:
-        plot_filename = os.path.splitext(results_filename)[0] + '.pdf'
+        if count_frames is True and parse_timestamps is True:
+            rfp, rfe = os.path.splitext(results_filename)
+            count_filename = rfp + '-count' + rfe
+            timestamp_filename = rfp + '-timestamp' + rfe
+        elif count_frames is True:
+            count_filename = results_filename
+            timestamp_filename = None
+        else:
+            # in this case, parse_timestamps = True; we already checked and eliminated the both false case
+            count_filename = None
+            timestamp_filename = results_filename
+    # define plot filename if frame counting was indicated
+    if count_filename is not None:
+        plot_filename = os.path.splitext(count_filename)[0] + '.pdf'
+    else:
+        plot_filename = None
 
     # if plot and exit requested, then do so
-    if plot_and_exit:
+    if plot_and_exit is True:
         file_frame_counts = read_frame_count_results(file_path=results_filename)
         if append_results is not None:
             for arfn in append_results:
@@ -298,15 +440,25 @@ def main(argv):
         root_directory=session_directory)
     # determine the files in the recording directory matching the filename format
     matching_files = find_files(recording_directories=recording_directories, file_name_format=recording_filename_format,
-                                camera_names=camera_names, drop_last_file=drop_last_file, first_file_index=first_file)
-    # run the frame count queries
-    file_frame_counts = get_video_stats(video_file_names=matching_files)
-    # write the frame count results to a CSV file and, optionally, a plot by pole
-    write_frame_count_results(results_dict=file_frame_counts,
-                              filename=results_filename, print_results=print_output)
-    if plot_output is True:
-        plot_frame_count_results(results_dict=file_frame_counts, filename=plot_filename,
-                                 session_info_filename=session_info_file_path)
+                                camera_names=camera_names, drop_last_file=drop_last_file, first_file_index=first_file,
+                                filter_filenames=input_filename_filters)
+
+    if count_frames is True:
+        # run the frame count queries
+        file_frame_counts = get_video_frame_counts(video_file_names=matching_files)
+        # write the frame count results to a CSV file
+        write_frame_count_results(results_dict=file_frame_counts,
+                                  filename=count_filename, print_results=print_output)
+        # plot frame count results, if indicated
+        if plot_output is True:
+            plot_frame_count_results(results_dict=file_frame_counts, filename=plot_filename,
+                                     session_info_filename=session_info_file_path)
+
+    if parse_timestamps is True:
+        # run the parse timestamp queries
+        file_frame_timestamps = get_video_frame_timestamps(video_file_names=matching_files)
+        # write the frame timestamp results to a CSV file
+        write_frame_timestamp_results(results_dict=file_frame_timestamps, filename=timestamp_filename)
 
 
 if __name__ == '__main__':
